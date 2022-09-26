48b349c083b65c3a1a65288a2fe7a05aa388d6db
Evyatar <evya7777777@gmail.com>
2022-09-27 00:54:12 +0300

    added http module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { CesiumDirective } from './cesium.directive';
 
 @NgModule({
   declarations: [AppComponent, CesiumDirective],
-  imports: [BrowserModule],
+  imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
